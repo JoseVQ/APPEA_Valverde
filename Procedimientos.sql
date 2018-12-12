e29a98583f6cc8f81b31e949b48ca68c00d5e4ff
JoseVQ <elvis.salvador.fa@outlook.com>
2018-12-12 16:03:13 -0500

    Backup Actualizado y Script SQL  de procedimientos

---
+++ Procedimientos.sql
@@ -0,0 +1,18 @@
+
+
+Create or ALTER   procedure [dbo].[pa_persona_listarTodos]
+as
+select  P.Nombres + P.Apellidop + P.Apellidom as [Nombre y Apellido], P.Direccion , P.Email , P.Edad
+from Persona P 
+
+exec pa_persona_listarTodos
+
+
+CREATE OR ALTER procedure [dbo].[pa_buscar_x_dni]
+@dni char(8)
+as
+select  P.Nombres + P.Apellidop + P.Apellidom as [Nombre y Apellido], P.Direccion , P.Email , P.Edad
+from Persona P 
+where P.dni=@dni
+
+EXEC [pa_buscar_x_dni] '12345678'
